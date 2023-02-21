030dcd61cfcfc61172a1c98c73351df1cfa6cb12
Park-kxng <coding.parkgy29@gmail.com>
2023-02-21 21:01:43 +0900

    feat : 네비게이션 바

---
+++ lib/widget/bottom_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Bottom extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      color: Colors.white,
+      child: Container(height: 50,
+        child: TabBar(
+          labelColor: Colors.orange,
+          unselectedLabelColor: Colors.blueGrey,
+          indicatorColor: Colors.transparent,
+          tabs: <Widget>[
+            Tab( icon : Icon(
+              Icons.home,
+              size: 18,
+            ),
+            child: Text('메인',
+              style: TextStyle(fontSize: 9),
+            ),),
+
+            Tab( icon : Icon(
+              Icons.home,
+              size: 18,
+            ),
+              child: Text('걷기',
+                style: TextStyle(fontSize: 9),
+              ),),
+
+            Tab( icon : Icon(
+              Icons.home,
+              size: 18,
+            ),
+              child: Text('마이 페이지',
+                style: TextStyle(fontSize: 9),
+              ),),
+
+
+
+          ],
+
+      ),
+      ),
+    );
+  }
+}
