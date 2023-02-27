382ccc69fa0fff40c3f434e0fe20f6dbffd2d5e3
dbsdlqls <yunibin99@gmail.com>
2023-02-27 22:36:34 +0900

    feat: google oauth2, jwt

---
+++ backend/largo/src/main/java/com/inandout/largo/auth/security/CustomOAuth2SuccessHandler.java
@@ -0,0 +1,36 @@
+package com.inandout.largo.auth.security;
+
+import com.inandout.largo.auth.jwt.JwtTokenProvider;
+import com.inandout.largo.user.domain.Role;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        Map<String,Object> attributes = oAuth2User.getAttributes();
+
+        String token = jwtTokenProvider.createToken(attributes.get("email").toString(), Role.USER);
+        String targetUrl = request.getParameter("redirect_uri") + "?token=" + token;
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+
+        log.info("created token {}", token);
+        log.info("targetUrl {}", targetUrl);
+    }
+}
---
+++ backend/largo/src/test/java/com/inandout/largo/LargoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inandout.largo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
