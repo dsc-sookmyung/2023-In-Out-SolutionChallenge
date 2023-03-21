d269df98b5bdcffb042d9ef1d11b73fbb045a56b
dbsdlqls <yunibin99@gmail.com>
2023-03-22 02:02:19 +0900

    feat: course save api

---
+++ backend/largo/src/main/java/com/inandout/largo/course/domain/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.inandout.largo.course.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
---
+++ backend/largo/src/main/java/com/inandout/largo/place/domain/UserPlace.java
@@ -0,0 +1,37 @@
+package com.inandout.largo.place.domain;
+
+import com.inandout.largo.course.domain.Course;
+import com.inandout.largo.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "user_place")
+@Entity
+public class UserPlace {
+    @Id
+    @Column(name = "up_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", referencedColumnName = "course_id", nullable = false)
+    private Course course;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String picture;
+
+    @Builder
+    public UserPlace(User user, Course course, String picture){
+        this.user = user;
+        this.course = course;
+        this.picture = picture;
+    }
+}
