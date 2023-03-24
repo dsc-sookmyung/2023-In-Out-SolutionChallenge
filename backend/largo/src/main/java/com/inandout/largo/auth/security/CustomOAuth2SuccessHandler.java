fb6d66e782b98599f2bcc87f808b6478da393ea9
dbsdlqls <yunibin99@gmail.com>
2023-03-24 18:54:03 +0900

    fix: login success redirect uri

--- backend/largo/src/main/java/com/inandout/largo/auth/security/CustomOAuth2SuccessHandler.java
+++ backend/largo/src/main/java/com/inandout/largo/auth/security/CustomOAuth2SuccessHandler.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -21,16 +22,18 @@ import java.util.Map;
 public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private final JwtTokenProvider jwtTokenProvider;
 
+    @Value("${flutter.uri-scheme}")
+    private String uri;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         Map<String,Object> attributes = oAuth2User.getAttributes();
 
         String token = jwtTokenProvider.createToken(attributes.get("email").toString(), Role.USER);
-        String targetUrl = "/login?token=" + token;
+        String targetUrl = uri+"://?route=main&email="+attributes.get("email")+"&token=" + token;
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
 
         log.info("created token {}", token);
-        log.info("targetUrl {}", targetUrl);
     }
 }
