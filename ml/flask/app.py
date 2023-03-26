5b2612c58f0c99980082136a85447f0971284e75
Jibin-Song <sjbpsh1@sookmyung.ac.kr>
2023-03-26 23:54:12 +0900

    feat : flask app.py 생성, 검색 test UI

---
+++ ml/flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template,request,redirect,url_for,g,session, make_response, jsonify, json
+from search_api import search
+import passage_retrieval_model
+
+
+app = Flask(__name__)
+app.register_blueprint(search)
+
+
+@app.route('/')
+def select():
+    return render_template('tag_test.html')
+
+ 
+if __name__ == "__main__":
+    print(passage_retrieval_model.main())
+    app.run(debug=True)
