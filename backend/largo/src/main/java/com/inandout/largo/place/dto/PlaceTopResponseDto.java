1a6b1758be17fe4ed1152f6a33e7be45f44ae7bf
dbsdlqls <yunibin99@gmail.com>
2023-03-27 01:45:40 +0900

    fix: top places response dto

--- backend/largo/src/main/java/com/inandout/largo/place/dto/PlaceTopResponseDto.java
+++ backend/largo/src/main/java/com/inandout/largo/place/dto/PlaceTopResponseDto.java
@@ -10,12 +10,15 @@ import java.util.stream.Collectors;
 public class PlaceTopResponseDto {
     private Long place_id;
     private String place_name;
+    private String address;
     private String picture;
     private List<String> hashtags;
 
     public PlaceTopResponseDto(Place entity){
         this.place_id = entity.getId();
         this.place_name = entity.getName();
+        this.address = entity.getAddressNum();
+        if(this.address == null || this.address.equals("")) this.address = entity.getAddressName();
         this.picture = entity.getPicture();
         this.hashtags = entity.getHashtags().stream()
                 .map(mapper -> mapper.getHashtag().getData())
