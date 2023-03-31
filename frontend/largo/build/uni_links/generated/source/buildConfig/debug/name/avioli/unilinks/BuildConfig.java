62e6e0c4c879c58912f7f872e345b52b4b25fa95
cherrrity <hyej0126@gmail.com>
2023-04-01 02:34:13 +0900

    feat : 폴더 정리

---
+++ frontend/largo/build/uni_links/generated/source/buildConfig/debug/name/avioli/unilinks/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package name.avioli.unilinks;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "name.avioli.unilinks";
+  public static final String BUILD_TYPE = "debug";
+}
